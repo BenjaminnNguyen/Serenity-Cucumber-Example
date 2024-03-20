e2c7513360eb1076b4a2260cd977b167a60f5a9c
Benjamin <nguyenquocbao.1999443@gmail.com>
2024-03-20 09:14:23 +0700

    update

--- src/main/java/cucumber/singleton/UrlAPI.java
+++
@@ -1,5 +0,0 @@
-package cucumber.singleton;
-
-public class UrlAPI {
-
-   }
--- src/test/java/starter/navigation/DuckDuckGoHomePage.java
+++
@@ -1,8 +0,0 @@
-package starter.navigation;
-
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("")
-public class DuckDuckGoHomePage extends PageObject {
-}
